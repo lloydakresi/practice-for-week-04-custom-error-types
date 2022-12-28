4f4366a5ddc0e0a8ea05f25f4c7b5801836a0936
Lloyd Akresi <lloydakresi03@gmail.com>
2022-12-28 22:09:15 +0000

    all tests passed

--- error-types/maximum-length-exceeded-error.js
+++ error-types/maximum-length-exceeded-error.js
@@ -1,6 +1,22 @@
 const ValidationError = require('./validation-error');
 
 // Your code here
+class MaximumLengthExceededError extends ValidationError{
+  constructor(excessLength, ...params){
+    super(...params);
+    this.name = "MaximumLengthExceededError";
+    if (ValidationError.captureStacktrace){
+      ValidationError.captureStacktrace(this, MaximumLengthExceededError);
+    }
+
+    if (excessLength !== undefined){
+      this.message = `Maximum length exceeded by ${excessLength}`;
+    }else{
+      this.message = `Maximum length exceeded`
+    }
+  }
+
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
--- error-types/validation-error.js
+++ error-types/validation-error.js
@@ -1,4 +1,16 @@
 // Your code here
+class ValidationError extends Error{
+  constructor(message, ...params){
+    super(...params);
+    this.name = "ValidationError";
+    this.message = message || "Invalid input";
+    if(Error.captureStackTrace){
+      Error.captureStackTrace(this, ValidationError)
+    };
+
+
+  }
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
